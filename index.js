310239567f698b670da037387b0e5a330a2475ff
Vishwas Tarende <vishwas007t@gmail.com>
2021-10-25 23:57:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+	currentActive++;
+
+	if (currentActive > circles.length) {
+		currentActive = circles.length;
+	}
+	update();
+	console.log(currentActive);
+});
+
+prev.addEventListener('click', () => {
+	currentActive--;
+
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+
+	update();
+});
+
+const update = () => {
+	circles.forEach((circle, index) => {
+		if (index < currentActive) {
+			circle.classList.add('active');
+		} else {
+			circle.classList.remove('active');
+		}
+	});
+};
